863c73897ebceb24e2450314eb8314d4e838f4f0
amareLD <lahirudarshana.007@gmail.com>
2024-12-12 17:10:59 +0530

    Setup radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,10 @@
+import React from 'react'
+import {Button} from '@radix-ui/themes';
+
 const IssuesPage = () => {
   return <div>Issues Page
 
-        
+        <Button>BUTTON</Button>
   </div>;
 };
 
